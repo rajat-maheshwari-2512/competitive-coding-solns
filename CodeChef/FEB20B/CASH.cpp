a2e4793347ba902e5a4cbc7db85f7c429362a016
rajat-maheshwari-2512 <54249328+rajat-maheshwari-2512@users.noreply.github.com>
2020-10-02 21:31:36 +0530

    Update CASH.cpp

--- CodeChef/FEB20B/CASH.cpp
+++ CodeChef/FEB20B/CASH.cpp
@@ -1,19 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define lli long long
 int main() 
 {
-    long long t;
-    cin>>t;
-    for(long long i=0;i<t;i++)
-    {
-        long long a,n,k,sum=0;
-        cin>>n>>k;
-        for(long long j=0;j<n;j++)
-        {
-            cin>>a;
-            sum+=(a%k);
-        }
-        cout<<sum%k<<endl;
-    }
+	ios_base::sync_with_stdio(false);
+    	cin.tie(NULL);
+    	lli t;
+    	cin>>t;
+    	for(lli i=0;i<t;i++)
+    	{
+        	lli a,n,k,sum=0;
+        	cin>>n>>k;
+        	for(lli j=0;j<n;j++)
+        	{
+            	cin>>a;
+            	sum+=(a%k);
+        	}
+        	cout<<sum%k<<endl;
+    	}
 	return 0;
 }
